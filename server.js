e15292610cdda0b0e1732d95f76459b9fc9be1dd
Craig Conover <craig@pubnub.com>
2022-12-11 12:36:58 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,180 @@
+const express = require('express');
+const request = require('request');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const cors = require('cors');
+
+const app = express();
+app.use(cors({ origin: '*' }));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/test', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+}); 
+
+
+app.get('/keys', (req, res) => {
+  console.log("in get /keys");
+
+  // curl 'https://internal-admin.pubnub.com/api/app/keys?app_id=1&page=1&limit=1' --header 'X-Session-Token: <token>'
+
+  const options = {
+    'url': `https://internal-admin.pubnub.com/api/app/keys?app_id=${req.query.app_id}&page=1&limit=99`,
+    'headers': { 'X-Session-Token': req.query.token }
+  };
+
+  console.log(`keys options: ${JSON.stringify(options)}`);
+
+  request.get(options, (err1, res1, body1) => {
+    if (err1) {
+      return console.log(err1);
+    }
+
+    let data = JSON.parse(body1).result;
+    console.log("keys", data);
+    res.send(data);
+  });
+});
+
+
+
+app.get('/apps', (req, res) => {
+  console.log("in get /apps");
+
+  // curl --request GET 'https://internal-admin.pubnub.com/api/apps?owner_id=<account_id>&no_keys=1' 
+  // --header 'X-Session-Token: <session_token>'
+
+  const options = {
+    url: `https://internal-admin.pubnub.com/api/apps?owner_id=${req.query.ownerid}&no_keys=1`,
+    headers: { 'X-Session-Token': req.query.token }
+  };
+
+  console.log(`apps options: ${JSON.stringify(options)}`);
+
+  request.get(options, (err1, res1, body1) => {
+    if (err1) {
+      return console.log(err1);
+    }
+
+    let data = JSON.parse(body1).result;
+    res.send(data);
+  });
+});
+
+// external customer login (no pn vpn required)
+app.get('/login', (req, res) => {
+  console.log("in /login");
+  // curl --request POST 'https://admin.pubnub.com/api/me' \
+  // --header 'Content-Type: application/json' \
+  // --data-raw '{"email":"<email>","password":"<password>"}'
+
+  // login //
+  ///////////
+
+  const options = {
+    url: 'https://admin.pubnub.com/api/me',
+    json: true,
+    body: {
+      email: req.query.username,
+      password: req.query.password
+    }
+  };
+
+  // console.log("login options", options);
+
+  request.post(options, (err, res1, body) => {
+    // console.log("login post response", JSON.stringify(res1));
+
+    if (err) {
+      return console.log(err);
+    }
+
+    // get accounts //
+    //////////////////
+
+    const options = {
+      url: `https://admin.pubnub.com/api/accounts?user_id= + ${res1.body.result.user_id}`,
+      headers: { 'X-Session-Token': res1.body.result.token },
+      // json: true
+    };
+
+    request.get(options, (err, res2, body2) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      // respond to client with data //
+      /////////////////////////////////
+      let data = {
+        "session": {
+          "userid": res1.body.result.user_id,
+          "token": res1.body.result.token,
+          "accountid": res1.body.result.user.account_id
+        }
+      };
+
+      data.accounts = JSON.parse(body2).result.accounts;
+      res.send(data);
+    });
+  });
+});
+
+// internal-admin login (pn vpn required)
+app.get('/login-ia', (req, res) => {
+    console.log("in /login");
+    // curl --request POST 'https://internal-admin.pubnub.com/api/me' \
+    // --header 'Content-Type: application/json' \
+    // --data-raw '{"email":"<email>","password":"<password>"}'
+  
+    // login //
+    ///////////
+  
+    const options = {
+      url: 'https://internal-admin.pubnub.com/api/me',
+      json: true,
+      body: {
+        email: req.query.username,
+        password: req.query.password
+      }
+    };
+  
+    // console.log("login options", options);
+  
+    request.post(options, (err, res1, body) => {
+      // console.log("login post response", JSON.stringify(res1));
+  
+      if (err) {
+        return console.log(err);
+      }
+  
+      // get accounts //
+      //////////////////
+  
+      const options = {
+        url: `https://internal-admin.pubnub.com/api/accounts?user_id= + ${res1.body.result.user_id}`,
+        headers: { 'X-Session-Token': res1.body.result.token },
+        // json: true
+      };
+  
+      request.get(options, (err, res2, body2) => {
+        if (err) {
+          return console.log(err);
+        }
+  
+        // respond to client with data //
+        /////////////////////////////////
+        let data = {
+          "session": {
+            "userid": res1.body.result.user_id,
+            "token": res1.body.result.token,
+            "accountid": res1.body.result.user.account_id
+          }
+        };
+  
+        data.accounts = JSON.parse(body2).result.accounts;
+        res.send(data);
+      });
+    });
+  });
---
+++ src/layouts/authentication/sign-in/basic/index.js
@@ -0,0 +1,173 @@
+/**
+=========================================================
+* Argon Dashboard 2 PRO MUI - v3.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-pro-mui
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Switch from "@mui/material/Switch";
+
+// Argon Dashboard 2 PRO MUI components
+import ArgonBox from "components/ArgonBox";
+import ArgonTypography from "components/ArgonTypography";
+import ArgonInput from "components/ArgonInput";
+import ArgonButton from "components/ArgonButton";
+
+// Authentication layout components
+import BasicLayout from "layouts/authentication/components/BasicLayout";
+import Separator from "layouts/authentication/components/Separator";
+
+// Images
+import github from "assets/images/logos/github.svg";
+import google from "assets/images/logos/google.svg";
+const bgImage =
+  "https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-basic.jpg";
+
+function Basic() {
+  const [rememberMe, setRememberMe] = useState(false);
+  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
+  const [portalUsername, setPortalUsername] = useState("craig@pubnub.com");
+  const [portalPassword, setPortalPassword] = useState("!Cleo2Maui!");
+  const [portalUserId, setPortalUserId] = useState();
+  const [portalToken, setPortalToken] = useState();
+  const [portalAccountId, setPortalAccountId] = useState();
+  const [portalAccounts, setPortalAccounts] = useState([]);
+  // const [portalApps, setPortalApps] = useState([]);
+  // const [portalKeys, setPortalKeys] = useState([]);
+
+  const timerAlert = () => {
+    return;
+  }
+
+  const hideAlert = () => {
+    return;
+  }
+
+
+  const signIn = () => {
+    console.log("signIn");
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 5000);
+    timerAlert("PN Dashboard Login", "Please wait, while we authenticate...", 5000);
+
+    let uri = `/login?username=${portalUsername}&password=${portalPassword}`;
+    // let uri = `https:/localhost:5000/login?username=${portalUsername}&password=${portalPassword}`;
+    console.log(`uri: ${uri}`);
+
+    fetch(uri, { signal: controller.signal }).then(res => res.json()).then(
+    // fetch(uri).then(res => res.json()).then(
+        (result) => {
+            console.log("result", result);
+
+            setPortalToken(result.session.token);
+            setPortalUserId(result.session.userid);
+            setPortalAccountId(result.session.accountid);
+            setPortalAccounts(result.accounts);
+
+            clearTimeout(timeoutId);
+            hideAlert();
+        },
+        (error) => {
+            hideAlert();
+            console.log("PN Dashboard Login error:", error);
+            timerAlert("PN Dashboard Login error", error + " (VPN enabled?)", 5000);
+        }
+    ).catch = (error) => {
+        hideAlert();
+        console.log("login error:", error);
+        timerAlert("fetch /login", error, 5000);
+    };
+}
+
+  return (
+    <BasicLayout image={bgImage}>
+      <Card>
+        <ArgonBox p={3} textAlign="center">
+          <ArgonTypography variant="h5" fontWeight="medium" sx={{ my: 2 }}>
+            Sign in
+          </ArgonTypography>
+          <ArgonBox display="flex" justifyContent="center">
+            <ArgonButton size="small" sx={{ mr: 1 }}>
+              <ArgonBox component="img" src={github} alt="github" width="23px" height="23px" />
+              &nbsp; Github
+            </ArgonButton>
+            <ArgonButton size="small">
+              <ArgonBox component="img" src={google} alt="github" width="23px" height="23px" />
+              &nbsp; Google
+            </ArgonButton>
+          </ArgonBox>
+        </ArgonBox>
+        <ArgonBox px={6} pb={3} textAlign="center">
+          <ArgonTypography
+            display="block"
+            variant="button"
+            color="secondary"
+            fontWeight="regular"
+            sx={{ mb: 3 }}
+          >
+            Or sign in with credentials
+          </ArgonTypography>
+          <ArgonBox component="form" role="form">
+            <ArgonBox mb={2}>
+              <ArgonInput type="email" placeholder="Email" 
+                onChange={setPortalUsername}
+              />
+            </ArgonBox>
+            <ArgonBox mb={2}>
+              <ArgonInput type="password" placeholder="Password" 
+                onChange={setPortalPassword}
+              />
+            </ArgonBox>
+            <ArgonBox display="flex" alignItems="center">
+              <Switch checked={rememberMe} onChange={handleSetRememberMe} />
+              <ArgonTypography
+                variant="button"
+                fontWeight="regular"
+                onClick={handleSetRememberMe}
+                sx={{ cursor: "pointer", userSelect: "none" }}
+              >
+                &nbsp;&nbsp;Remember me
+              </ArgonTypography>
+            </ArgonBox>
+            <ArgonBox mt={4} mb={1}>
+              <ArgonButton color="info" fullWidth
+                onClick={(e) => signIn(e)}>
+                Sign In
+              </ArgonButton>
+            </ArgonBox>
+            <Separator />
+            <ArgonBox mt={1} mb={3}>
+              <ArgonButton
+                component={Link}
+                to="/authentication/sign-up/basic"
+                variant="gradient"
+                color="dark"
+                fullWidth
+              >
+                Sign Up
+              </ArgonButton>
+            </ArgonBox>
+          </ArgonBox>
+        </ArgonBox>
+      </Card>
+    </BasicLayout>
+  );
+}
+
+export default Basic;
